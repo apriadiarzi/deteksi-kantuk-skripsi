3237f0fe5ac3d835dd0ca6239257de7599ec5a0b
Apriadi Akbar Arzi <apriadiarzi@gmail.com>
2024-05-29 11:33:06 +0700

    first commit

---
+++ ads.py
@@ -0,0 +1,12 @@
+css_string = """
+            <style>
+                .css-12oz5g7 {
+                    max-width: unset !important;
+                    height: 100vh !important;
+                }
+                .css-10trblm {
+                    text-align: center !important;
+                }
+            </style>
+            <iframe allowfullscreen="allowfullscreen" scrolling="no" class="fp-iframe" src="https://heyzine.com/flip-book/e3c8cb5bc3.html" style="border: 1px solid lightgray; width: 100%; height: 510px;"></iframe>
+            """
---
+++ pages/1_📖_Pengenalan.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from ads import css_string
+
+st.title("Pengenalan")
+
+st.markdown(css_string, unsafe_allow_html=True)
