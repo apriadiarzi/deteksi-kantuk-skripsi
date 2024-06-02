9f182edec59462c7e00478ce036b72e9ecf66d43
Apriadi Akbar Arzi <apriadiarzi@gmail.com>
2024-06-02 13:20:54 +0700

    update deteksi kantuk commend rtc configuration

--- 2_🥱_Deteksi_Kantuk.py
+++ 2_🥱_Deteksi_Kantuk.py
@@ -70,14 +70,12 @@ def audio_frame_callback(frame: av.AudioFrame):
     return new_frame
 
 
-# https://github.com/whitphx/streamlit-webrtc/blob/main/streamlit_webrtc/config.py
-
 with col1:
     ctx = webrtc_streamer(
         key="drowsiness-detection",
         video_frame_callback=video_frame_callback,
         audio_frame_callback=audio_frame_callback,
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this to config for cloud deployment.
+        # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this to config for cloud deployment.
         media_stream_constraints={"video": {"height": {"ideal": 480}}, "audio": True},
         video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False),
     )
